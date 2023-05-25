28d50adcd67eda493fd4d18ae2faab305e934845
Khaled_Dawoud <kdawoud56@gmail.com>
2023-05-25 17:54:31 +0300

    fixed submission and validation, added forget password and register labels

--- MUNSiteCode/Users/forms.py
+++ MUNSiteCode/Users/forms.py
@@ -1,11 +1,12 @@
-from flask_wtf import FlaskForm
+from flask import current_app
+from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email
 import email_validator
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email address", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember me")
-    submit = SubmitField("Login")
+    submit = SubmitField("Sign In")
--- MUNSiteCode/Users/routes.py
+++ MUNSiteCode/Users/routes.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, url_for, render_template
+from flask import Blueprint, url_for, render_template, redirect
 from MUNSiteCode.Users.forms import LoginForm
 
 users = Blueprint("users", __name__)
 
 
-@users.route("/login")
+@users.route("/login", methods=["POST", "GET"])
 def login():
     template_url = url_for('users.login')
     form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('main.home'))
     return render_template("login.html", template_url=template_url, form=form)
